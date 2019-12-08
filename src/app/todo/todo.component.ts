cb984c850136b36337411fd1e7987acfb67a7fa8
Marcin Jagiela <marcin.jagiela@hotmail.com>
2019-12-08 10:46:54 +0100

    done, now errors

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,6 @@
 import { TodoService } from './../todo.service';
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -8,7 +8,11 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  newTodo = '';
+  @ViewChild('todoForm', null) todoForm: NgForm;
+  newTodo = {
+    subject: '',
+    dueDate: null
+  };
   todos = [];
   itemDeleted(id) {
    this.todoService.delete(id)
@@ -22,13 +26,15 @@ export class TodoComponent implements OnInit {
     .subscribe();
   }
   itemAdded() {
-    this.todoService.create({
-      subject: this.newTodo
-    })
+    this.todoService.create(this.newTodo)
     .subscribe(
       response => {
         this.todos.push(response);
-        this.newTodo = '';
+        this.newTodo = {
+          subject: '',
+          dueDate: null
+        };
+        this.todoForm.reset();
       }
     );
   }
